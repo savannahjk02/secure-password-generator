8ef2871dd62658378fa8e63dbf3acd27343b94f3
savannahjk02 <savannahjk02@gmail.com>
2023-10-10 23:19:20 -0400

    added javascript

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  //create function (function declaration/expression)
+}
+
+var numOfCharacters = prompt ("How many characters do you want?");
+
+numOfCharacters = parseInt(numOfCharacters);
+
+// 4 ifs, 4 confirmExpressionconditionals
+
+
+// processing... (random generate characters)
+// return password
+
+function generatePassword(){
+  return ("random Password")
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +27,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
